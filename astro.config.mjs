324a08cb53052c25fcbce236adb0ac72c57de09d
Nora Mate <norama.matema@gmail.com>
2023-07-10 00:32:03 +0200

    NameInput as MF

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config'
+import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  integrations: [
+    solid(),
+    {
+      name: 'importmap-externals',
+
+      hooks: {
+        'astro:build:setup': ({ vite, target }) => {
+          if (target === 'client') {
+            vite.build.rollupOptions['external'] = ['solid-js', '@modular-forms/solid']
+          }
+        },
+      },
+    },
+  ],
 })
---
+++ src/microfrontends/NameInput.tsx
@@ -0,0 +1,6 @@
+// @ts-ignore
+import NameInput from 'http://localhost:7100/bundle.js'
+
+const MF = () => <NameInput />
+
+export default MF
